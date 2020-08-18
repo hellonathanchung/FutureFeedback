a51b733a2b588ef4bc608691b3915b946bba24ff
afteralec <github email address>
2020-08-17 17:27:24 -0700

    Adds welcome controller and custom /login path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  root 'posts#index'
+  root 'welcome#index'
 
   resources :posts
   devise_for :users
+
+  devise_scope :user do
+    get '/login' => 'devise/sessions#new' # Custom devise path for login
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
