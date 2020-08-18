0427c6d2ba9cef48864a78e6d191216d6d6aae90
afteralec <github email address>
2020-08-17 17:44:54 -0700

    Adds role to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,16 @@
 class User < ApplicationRecord
+  before_validation :set_default_role
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum :role [ :user, :moderator, :administrator ] # creates Devise roles
+
+  private
+
+  def set_default_role
+    self.role ||= :user
+  end
 end
