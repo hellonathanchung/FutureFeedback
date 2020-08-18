cbe227e448dab42c34fc64ad7b88c5325d7856be
Nathan <chung.nathanh@gmail.com>
2020-08-18 11:48:52 -0700

    reworks post_tag associations and creates seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+##### tags ####
+feature = Tag.new(name:feature)
+bug = Tag.new(name:bug)
