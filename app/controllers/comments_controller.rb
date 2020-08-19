f586199a350007ce813b99503dfe8f411ba1e41a
Nathan <chung.nathanh@gmail.com>
2020-08-18 17:15:52 -0700

    creates comments resources

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,11 @@
 class CommentsController < ApplicationController
+    before_action :find_post
+    
+    
+    
+    private
+    def find_post
+        @post = Post.find(params[:id])
+    end
+
 end
