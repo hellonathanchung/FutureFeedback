04027852520942023bd1057d95807017696443c6
afteralec <github email address>
2020-08-20 12:21:27 -0700

    Will finish the tags forms and update forms across site

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -4,4 +4,12 @@ class Tag < ApplicationRecord
 
   validates :name, length: { minimum: 1, maximum: 10 }
   validates_uniqueness_of :name
+
+  before_validation :downcase_name
+
+  private
+
+  def downcase_name
+    self.name = self.name.downcase
+  end
 end
