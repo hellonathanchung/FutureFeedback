179c43a6c2f4395675d36e89faad71b3967a7eea
afteralec <github email address>
2020-08-20 11:50:30 -0700

    Will correct minimum on tag.rb validation

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,6 +2,6 @@ class Tag < ApplicationRecord
   has_many :post_tags, dependent: :destroy
   has_many :posts, through: :post_tags
 
-  validates :name, length: { minimum: 0, maximum: 10 }
+  validates :name, length: { minimum: 1, maximum: 10 }
   validates_uniqueness_of :name
 end
