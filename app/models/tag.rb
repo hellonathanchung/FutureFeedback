dd316e33b5992c76acc04c6f18a5711211a4a3fb
afteralec <github email address>
2020-08-20 11:49:28 -0700

    Will complete MVP of tags index

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,7 @@
 class Tag < ApplicationRecord
-  has_many :post_tags
+  has_many :post_tags, dependent: :destroy
   has_many :posts, through: :post_tags
+
+  validates :name, length: { minimum: 0, maximum: 10 }
+  validates_uniqueness_of :name
 end
